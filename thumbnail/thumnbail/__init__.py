61d6a6d2ea34f2598d4b4d86b9eaac8d2c3f50cf
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-05-23 23:55:46 +0530

    setup -- started using a result backend for chord workflow

--- thumbnail/thumnbail/__init__.py
+++ thumbnail/thumnbail/__init__.py
@@ -5,14 +5,16 @@ from .db import create_db
 
 from thumnbail.workers.BgWorkers import create_bg_workers
 CELERY_BROKER_BACKEND = os.environ.get("AMQP_URI")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
         CELERY=dict(
-            broker_url=CELERY_BROKER_BACKEND, task_track_started=True,
-            celery_imports = ("thumbnail.workers",)
+            broker_url=CELERY_BROKER_BACKEND, result_backend=CELERY_RESULT_BACKEND,
+            task_track_started=True,
+            celery_imports=("thumbnail.workers",)
         )
     )
     app.register_blueprint(router)

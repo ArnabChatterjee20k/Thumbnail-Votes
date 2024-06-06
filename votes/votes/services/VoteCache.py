2b4403d6650bc2f69f26ed21c256032e15219b3e
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-06-07 01:51:36 +0530

    feat -- added more methods to work with vote and caching them

--- votes/votes/services/VoteCache.py
+++ votes/votes/services/VoteCache.py
@@ -1,5 +1,5 @@
 from typing import Iterator
-
+from votes.utils.vote import get_vote,get_voters
 class VoteManager:
     _instance = None
 
@@ -12,8 +12,8 @@ class VoteManager:
     def __init__(self):
         self._votes = {}
 
-    def __getitem__(self, vote_id):
-        return self._votes.__getitem__(vote_id)
+    def __getitem__(self, project_id):
+        return self._votes.__getitem__(project_id)
 
     def __setitem__(self, key, value):
         self._votes.__setitem__(key, value)
@@ -24,7 +24,35 @@ class VoteManager:
     def __len__(self) -> int:
         return self._votes.__len__()
 
-    def pop(self, vote_id):
-        if self.__getitem__(vote_id):
-            return self._votes.pop(vote_id)
+    def __repr__(self) -> str:
+        return str(self._votes)
+
+    def pop(self, project_id):
+        if project_id in self._votes:
+            return self._votes.pop(project_id)
         return None
+
+    def cache_votes(self,project_id):
+        if project_id in self._votes:
+            return
+        thumbnails = self._cache_thumbnails(project_id)
+        for thumbnail in thumbnails:
+            self._cache_voters(project_id,thumbnail.thumbnail_id)
+            return True
+        
+        return False
+
+    def _cache_thumbnails(self,project_id):
+        try:
+            vote = get_vote(project_id=project_id)
+            if vote:
+                self._votes[project_id] = {data.thumbnail_id:[] for data in vote}
+            return vote
+        except Exception as e:
+            print(e)
+            return None
+
+    def _cache_voters(self,project_id,thumbnail_id):
+        voters = get_voters(thumbnail_id=thumbnail_id)
+        for voter in voters:
+            self._votes[project_id][voter.thumbnail_voted].append(voter.user_id)

bea3f62d64808e6c53919c7edebe4e976dcc94ae
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-07-13 12:25:09 +0530

    feat -- added project creation panel

---
+++ ui/src/app/vote/page.tsx
@@ -0,0 +1,4 @@
+import { CreateProject } from "@/components/create-project";
+export default async function Vote() {
+  return <CreateProject />;
+}
---
+++ ui/src/components/user-avatar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { signOut, useSession } from "next-auth/react";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+export default function UserAvatar() {
+  const { data, status } = useSession();
+  return (
+    <Menubar className="outline-transparent border-none rounded-full">
+      <MenubarMenu>
+        <MenubarTrigger>
+          <Avatar className="hover:border-2  hover:border-white transition-all duration-75 cursor-pointer">
+            {<AvatarImage src={data?.user?.image as string} />}
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </MenubarTrigger>
+        <MenubarContent className="bg-black outline-transparent border-none">
+          <MenubarItem onClick={signOut} className="hover:bg-zinc-700 cursor-pointer">Logout</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+}
